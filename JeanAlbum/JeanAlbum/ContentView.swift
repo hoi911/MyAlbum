b01e19d1191ff32231545674006f22767b635077
hoi911 <nextix88@gmail.com>
2022-08-17 17:53:21 +0900

    initial commit

---
+++ JeanAlbum/JeanAlbum/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  JeanAlbum
+//
+//  Created by 장진영 on 2022/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JeanAlbum/JeanAlbum/JeanAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeanAlbumApp.swift
+//  JeanAlbum
+//
+//  Created by 장진영 on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct JeanAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
